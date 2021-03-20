ebd19012b73b6c114f581d8d0f5c6df7cbc28861
czh <549425707@qq.com>
2021-03-20 15:45:42 +0800

    111

--- src/main/java/com/czh/pojo/User.java
+++ src/main/java/com/czh/pojo/User.java
@@ -1,6 +1,6 @@
 package com.czh.pojo;
 
-import sun.rmi.server.InactiveGroupException;
+
 
 public class User {
 
@@ -19,5 +19,85 @@ public class User {
 
     private String bbb;
 
+    public User() {
+    }
 
+    public User(String name, Integer age, String sex, Long aaa, String test, String zzz, String bbb) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+        this.aaa = aaa;
+        this.test = test;
+        this.zzz = zzz;
+        this.bbb = bbb;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Long getAaa() {
+        return aaa;
+    }
+
+    public void setAaa(Long aaa) {
+        this.aaa = aaa;
+    }
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+
+    public String getZzz() {
+        return zzz;
+    }
+
+    public void setZzz(String zzz) {
+        this.zzz = zzz;
+    }
+
+    public String getBbb() {
+        return bbb;
+    }
+
+    public void setBbb(String bbb) {
+        this.bbb = bbb;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", sex='" + sex + '\'' +
+                ", aaa=" + aaa +
+                ", test='" + test + '\'' +
+                ", zzz='" + zzz + '\'' +
+                ", bbb='" + bbb + '\'' +
+                '}';
+    }
 }
