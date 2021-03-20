df4bc41ff5e51039b1696e415c059f3573cce761
czh <549425707@qq.com>
2021-03-20 15:22:31 +0800

    111

--- src/main/java/com/czh/pojo/User.java
+++ src/main/java/com/czh/pojo/User.java
@@ -1,10 +1,15 @@
 package com.czh.pojo;
 
+import sun.rmi.server.InactiveGroupException;
+
 public class User {
 
     private String name;
 
     private Integer age;
 
+    private String sex;
+
+    private Long aaa;
 
 }
