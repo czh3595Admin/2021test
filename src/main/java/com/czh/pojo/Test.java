a778e50f4367a5352db1994e789d1808d5d19622
czh <549425707@qq.com>
2021-03-20 15:54:45 +0800

    Merge branch 'master' of https://github.com/czh3595Admin/2021test
    # Conflicts:
    #	src/main/java/com/czh/pojo/User.java

---
+++ src/main/java/com/czh/pojo/Test.java
@@ -0,0 +1,9 @@
+package com.czh.pojo;
+
+public class Test {
+    public static void main(String[] args) {
+        User user = new User();
+        user.setAaa(1L);
+        System.out.println(user);
+    }
+}
